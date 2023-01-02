7712fa380da7ed352a25882194639a09101deac0
Radvil <radvil@access-mobile.com>
2023-01-03 04:28:18 +0800

    Configure object and perspective camera

--- src/app/learn-threejs/lib/product-preview/product-preview.component.ts
+++ src/app/learn-threejs/lib/product-preview/product-preview.component.ts
@@ -1,12 +1,26 @@
 import { NgtCanvasModule } from '@angular-three/core';
 import { NgtColorAttribute } from '@angular-three/core/attributes';
+import {
+    NgtAmbientLightModule,
+    NgtPointLightModule
+} from '@angular-three/core/lights';
 import { NgtPrimitiveModule } from '@angular-three/core/primitive';
+import { NgtSobaOrbitControlsModule } from '@angular-three/soba/controls';
 import { NgtGLTFLoader } from '@angular-three/soba/loaders';
-import { ComponentStore } from '@ngrx/component-store';
-import { LetModule, PushModule } from '@ngrx/component';
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { LetModule, PushModule } from '@ngrx/component';
+import { ComponentStore } from '@ngrx/component-store';
 import { switchMap } from 'rxjs';
+import {
+    Mesh,
+    MeshStandardMaterial,
+    Object3D,
+    PerspectiveCamera
+} from 'three';
+import { OrbitControls } from 'three-stdlib';
+
+type HexString = `#${string}`;
 
 interface State {
   fileModelPath: string;
@@ -26,6 +40,9 @@ const intitialState: State = {
     NgtCanvasModule,
     NgtColorAttribute,
     NgtPrimitiveModule,
+    NgtAmbientLightModule,
+    NgtPointLightModule,
+    NgtSobaOrbitControlsModule,
     LetModule,
     PushModule,
   ],
@@ -38,6 +55,44 @@ export class ProductPreviewComponent extends ComponentStore<State> {
   readonly cupCoffee$ = this.fileModelPath$.pipe(
     switchMap((path) => this.gltfLoader.load(path))
   );
+
+  #color: HexString = '#ff0000';
+
+  @Input()
+  set color(value: HexString) {
+    this.#color = value;
+    this.#applyColorToMaterial();
+  }
+
+  get color() {
+    return this.#color;
+  }
+
+  cupMaterial?: MeshStandardMaterial;
+
+  onObjectReady(object: Object3D): void {
+    const mesh = object.getObjectByName('Object_2') as Mesh;
+    this.cupMaterial = mesh.material as MeshStandardMaterial;
+    this.#applyColorToMaterial();
+  }
+
+  onOrbitControlReady(orbitControls: OrbitControls): void {
+    orbitControls.enableZoom = true;
+    orbitControls.autoRotate = true;
+    orbitControls.autoRotateSpeed = 9;
+    orbitControls.target.set(0, 0.5, 0);
+    orbitControls.object.position.setY(3);
+
+    const camera = orbitControls.object as PerspectiveCamera;
+    camera.zoom = 3;
+    camera.receiveShadow = true; // idk what is this
+		console.log(orbitControls)
+  }
+
+  #applyColorToMaterial(): void {
+    const hexColor = parseInt(this.color.substring(1), 16);
+    this.cupMaterial?.color.setHex(hexColor);
+  }
 
   constructor() {
     super(intitialState);
