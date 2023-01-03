e1a220c16350e628b81ba65925792443168c520e
Radvil <radvil@access-mobile.com>
2023-01-03 15:06:08 +0800

    Refactor routes structure

--- src/app/learn-threejs/page/learn-threejs.component.ts
+++ src/app/learn-threejs/page/learn-threejs.component.ts
@@ -1,30 +1,21 @@
-import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { LetModule } from '@ngrx/component';
-import { BehaviorSubject, distinctUntilChanged } from 'rxjs';
-import { ProductPreviewComponent } from 'src/app/learn-threejs/lib';
-
-type HexValue = `#${string}`;
-
-interface ColorOption {
-	label: string;
-	value: HexValue;
-}
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 export interface RouteNavigation {
 	link: string;
 	title: string;
+	icon: string;
 }
 
 export const RouteNavigations: RouteNavigation[] = [
 	{
 		link: 'mug-editor',
 		title: 'Mug Editor',
+		icon: 'code',
 	},
 ];
 
@@ -37,49 +28,11 @@ export const RouteNavigations: RouteNavigation[] = [
 	imports: [
 		CommonModule,
 		RouterModule,
-		ProductPreviewComponent,
-		MatCardModule,
-		MatButtonModule,
 		MatToolbarModule,
-		MatDividerModule,
-		LetModule,
+		MatButtonModule,
+		MatIconModule,
 	],
 })
 export class LearnThreejsComponent {
-	readonly colorOptions: ColorOption[] = [
-		{
-			label: 'Red',
-			value: '#cc0000',
-		},
-		{
-			label: 'Green',
-			value: '#1ee234',
-		},
-		{
-			label: 'Blue',
-			value: '#006bff',
-		},
-		{
-			label: 'Gold',
-			value: '#ffd700',
-		},
-		{
-			label: 'Black',
-			value: '#181818',
-		},
-	];
-
 	readonly navigations = RouteNavigations;
-
-	readonly #selectedColor = new BehaviorSubject<ColorOption>(
-		this.colorOptions[0]
-	);
-
-	readonly selectedColor$ = this.#selectedColor
-		.asObservable()
-		.pipe(distinctUntilChanged());
-
-	changeColor(option: ColorOption): void {
-		this.#selectedColor.next(option);
-	}
 }
--- src/app/learn-threejs/page/mug-editor/mug-editor.component.ts
+++ src/app/learn-threejs/page/mug-editor/mug-editor.component.ts
@@ -1,9 +1,65 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { LetModule } from '@ngrx/component';
+import { ProductPreviewComponent } from 'src/app/learn-threejs/lib';
+import { BehaviorSubject, distinctUntilChanged } from 'rxjs';
+
+type HexValue = `#${string}`;
+
+interface ColorOption {
+	label: string;
+	value: HexValue;
+}
 
 @Component({
 	standalone: true,
 	selector: 'app-mug-editor',
 	templateUrl: './mug-editor.component.html',
 	styleUrls: ['./mug-editor.component.scss'],
+	imports: [
+		CommonModule,
+
+		MatCardModule,
+		MatDividerModule,
+		ProductPreviewComponent,
+		LetModule,
+	]
 })
-export class MugEditorComponent { }
+export class MugEditorComponent { 
+	readonly colorOptions: ColorOption[] = [
+		{
+			label: 'Red',
+			value: '#cc0000',
+		},
+		{
+			label: 'Green',
+			value: '#1ee234',
+		},
+		{
+			label: 'Blue',
+			value: '#006bff',
+		},
+		{
+			label: 'Gold',
+			value: '#ffd700',
+		},
+		{
+			label: 'Black',
+			value: '#181818',
+		},
+	];
+
+	readonly #selectedColor = new BehaviorSubject<ColorOption>(
+		this.colorOptions[0]
+	);
+
+	readonly selectedColor$ = this.#selectedColor
+		.asObservable()
+		.pipe(distinctUntilChanged());
+
+	changeColor(option: ColorOption): void {
+		this.#selectedColor.next(option);
+	}
+}
